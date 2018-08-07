ff38570a9867323748bc5d76311409035644fab4
Yicheng Jin <bobjin42@gmail.com>
2018-08-07 03:59:09 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,9 +1,9 @@
 def count_elements(array)
   new_hash = {}
   array.each{|value|
-  if new_hash[:value] = nil
-    new_hash[:value] = 1
-  else new_hash[:value] += 1
+  if new_hash[value] == nil
+    new_hash[value] = 1
+  else new_hash[value] += 1
   end  
   }
   new_hash
